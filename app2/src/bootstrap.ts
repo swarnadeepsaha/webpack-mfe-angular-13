3e89d6f89382558c85ba09fa3bfa610f694d3fae
Swarnadeep <swarnadeep.saha@hotmail.com>
2022-09-27 16:03:08 +0530

    bootstrap using angular mfe tools and lerna start

--- app2/src/bootstrap.ts
+++ app2/src/bootstrap.ts
@@ -1,6 +1,6 @@
 import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-
+import { bootstrap } from '@angular-architects/module-federation-tools';
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 
@@ -8,5 +8,4 @@ if (environment.production) {
   enableProdMode();
 }
 
-platformBrowserDynamic().bootstrapModule(AppModule)
-  .catch(err => console.error(err));
+bootstrap(AppModule, { production: environment.production });
